68fdf911ac9f62597a6ef4851743b9e16b218bfc
Rob Moore <giftiger.wunsch@xantoria.com>
2018-12-15 13:32:05 +0000

    Alias functions

---
+++ functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+_scrabble_anagram() {
+  "$HOME/Code/tiny/anagram-finder/find.py" "$@"
+}
+
+_scrabble_like() {
+  grep -E "$1" "$HOME/Code/tiny/anagram-finder/scrabble-words.txt"
+}
+
+scrabble() {
+  case "$1" in
+    anagram)
+      shift
+      _scrabble_anagram "$@"
+      ;;
+    like)
+      shift
+      _scrabble_like "$@"
+      ;;
+    *)
+      _scrabble_anagram "$@"
+      ;;
+  esac
+}
